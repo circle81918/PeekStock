00302315ff16f62b9238ef487ab0573cfe0f8b76
Renee <circle81918@gmail.com>
2020-05-03 10:24:11 +0800

    Add background.js

---
+++ background.js
@@ -0,0 +1,53 @@
+setInterval(updateStock, 5000);
+
+function addStock(stockID){
+	if(stockID){
+		chrome.storage.local.get(stockID, function(data) {
+			if (typeof data[stockID] == 'undefined'){
+				var defaultValue = {name : stockID, currentPrice : 0.0, change : 0.0};
+				data[stockID] = defaultValue;
+				chrome.storage.local.set(data);
+			}
+			else{
+				alert(stockID + " already exists! ");		
+			}
+		});
+	}
+}
+
+function removeStock(stockID){
+	chrome.storage.local.remove(stockID);
+}
+
+function updateStock(){
+	chrome.storage.local.get(null, function(stockList){
+		let queryUrl = "http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=";
+		for (stockID in stockList) { queryUrl += "tse_" + stockID + ".tw|" }
+		$.ajax({ 
+            type: 'GET',
+            url:queryUrl, 
+            success: function (data) {
+				let stockArray = JSON.parse(data);
+				let stockData = {};
+				$.each(stockArray.msgArray, function(index, stock) { 
+					if(stock.z){
+						let changePercent = (stock.z == stock.y ) ? 0.0 : (stock.z - stock.y) / stock.y * 100;
+						let updateValue = {name : stock.n, currentPrice : stock.z, change : changePercent};
+						stockData[stock.c] = updateValue;
+					}
+				});
+				chrome.storage.local.set(stockData);
+				//callback();
+            }
+        });
+	});
+}
+
+function clearLocalStorage(){
+	chrome.storage.local.clear(function() {
+	  	var error = chrome.runtime.lastError;
+		if (error) {
+		  alert(error);
+		}
+	});
+}
--- popup.js
+++ popup.js
@@ -1,73 +1,37 @@
-setInterval(function(){ getStock(); }, 5000);
+var bg = chrome.extension.getBackgroundPage();
+
+// setInterval( function() {
+// 	updateStockTable(function(){
+// 		location.reload();
+// 	});
+// }, 5000);
+
+$(document).ready(function(){
+	updateStockTable();
+})
 
 document.addEventListener('click',function(e){
     if(e.target.id == 'remove'){
-        removeStock(e.target.value);
+		bg.removeStock(e.target.value);
+		location.reload();
 	 }
 	 if(e.target.id == 'add'){
-		addStock();
+		var stockID = document.getElementById("stockID").value;
+		bg.addStock(stockID);
 	 }
  });
 
-$(document).ready(function(){
-	getStock();
-})
-
-function addStock(){
-	var inputStockID = document.getElementById("stockID").value;
-	if(inputStockID){
-		chrome.storage.local.get('stockList', function(data) {
-			let setStockList = new Set(data.stockList);
-			setStockList.add(inputStockID);
-			let arrayStockList = Array.from(setStockList);
-			chrome.storage.local.set({stockList:arrayStockList});
-			location.reload();
-		});
-	}
-}
-
-function getStock(){
-	chrome.storage.local.get('stockList', function(data) {
-		let stockList = new Set(data.stockList);
-		getStockInfo(stockList);
-	});
-}
-
-function removeStock(stockID){
-	chrome.storage.local.get('stockList', function(data) {
-		let setStockList = new Set(data.stockList);
-		setStockList.delete(stockID);
-		let arrayStockList = Array.from(setStockList);
-		chrome.storage.local.set({stockList:arrayStockList});
-		location.reload();
-	});
-}
-
-function getStockInfo(stockList){
-	var queryUrl = "http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch="
-	stockList.forEach(stockID => {
-		queryUrl += "tse_" + stockID + ".tw|"
-	});
-	$.ajax({ 
-		type: 'GET',
-		url: queryUrl,
-		success: function (data) {
-			var stockArray = JSON.parse(data);
-			var stockValue = "";
-			$.each(stockArray.msgArray, function(index, stock) { 
-				var percent = (stock.z - stock.y) / stock.y * 100
-				stockValue  += `<tr><td>${stock.n}</td><td>${stock.z}</td><td>${String(percent).slice(0,3)}</td><td><button class="btn btn-outline-danger btn-sm" id='remove' value="${stock.c}">Remove</button></td></tr>`;
-			});
-			document.getElementById("table_body").innerHTML = stockValue;
-		},
-	}); 
-}
-
-function clearLocalStorage(){
-	chrome.storage.local.clear(function() {
-	  var error = chrome.runtime.lastError;
-		if (error) {
-		  alert(error);
+ function updateStockTable(){
+	chrome.storage.local.get(null, function(items){
+		var stockData = "";
+		for (key in items)
+		{
+			stockData  += `<tr><td>${items[key].name}</td><td>${items[key].currentPrice}</td><td>${String(items[key].change).slice(0,3)}</td><td><button class="btn btn-outline-danger btn-sm" id='remove' value="${key}">Remove</button></td></tr>`;
 		}
-	 })
-   }
+		document.getElementById("table_body").innerHTML = stockData;
+	});
+ }
+
+ chrome.storage.onChanged.addListener(function(changes,areaName) {
+	updateStockTable();
+});
